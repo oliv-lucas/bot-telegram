c48177d8d8e2d39ce3157c3cdb3a775910adf225
oliv-lucas <36717061+oliv-lucas@users.noreply.github.com>
2018-02-21 23:24:41 -0300

    Update chamadahtml.php

--- chamadahtml.php
+++ chamadahtml.php
@@ -22,4 +22,38 @@ function getResult($lottery, $title){
 	}
 	return $out;
 }
+
+function parser($url){
+	//obtém o html da página
+	$html = file_get_html($url);
+	if (!empty($html)) {
+		$concurso = $html->find('span.numero-concurso',0)->plaintext;
+		$data = $html->find('span.data-concurso',0)->plaintext;
+		if(!isset($html->find('div[class="dados-nao-acumulou desativado"]',0)->plaintext)){
+			$acumulado = $html->find('span.label-valor-acumulado',0)->plaintext." ".$html->find('span.valor-acumulado',0)->plaintext;
+		}else{
+			$acumulado = "Não Acumulou!";
+		}
+		$numeros = "";
+		foreach ($html->find('span.numero-sorteado') as $numero) {
+			$numeros .= $numero->plaintext . "  ";
+		}
+		$premios = "";
+		foreach ($html->find('tr.premio') as $premio) {
+			$premios .= "\n" . $premio->find('td.label-premio',0)->plaintext . " - " . $premio->find('td.ganhadores-premio',0)->plaintext;
+			if ($premio->find('td.rateio-premio',0)->plaintext != "-") {
+				$premios .= " - ".$premio->find('td.rateio-premio',0)->plaintext;
+			}
+		}
+		return "\n---------------".
+		"\n".$concurso .
+		"\nDATA: " . $data .
+		"\nNÚMEROS:  " . $numeros .
+		"\n".$acumulado.
+		"\n---------------".
+		"\nPREMIAÇÕES" . $premios;
+	}else{
+		return "\nNão encontrado";
+	}
+}
 ?>
