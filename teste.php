8a4af7c90de12ba4a60439cddd84b123b7f43935
oliv-lucas <36717061+oliv-lucas@users.noreply.github.com>
2018-02-25 00:35:04 -0300

    Add files via upload

---
+++ teste.php
@@ -0,0 +1,20 @@
+<?php 
+define('BOT_TOKEN', 'YOURBOT:TOKEN');
+define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
+	
+// read incoming info and grab the chatID
+$content = file_get_contents("php://input");
+$update = json_decode($content, true);
+$chatID = $update["message"]["chat"]["id"];
+		
+// compose reply
+$reply =  sendMessage();
+		
+// send reply
+$sendto =API_URL."sendmessage?chat_id=".$chatID."&text=".$reply;
+file_get_contents($sendto);
+
+function sendMessage(){
+$message = "I am a baby bot.";
+return $message;
+}
